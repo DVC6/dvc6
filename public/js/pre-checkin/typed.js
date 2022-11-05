f33786f730753c5e0b7fc79095c18c45ac1da65a
Gabriel da Silva <gabriel.dasilva2880@gmail.com>
2022-11-05 18:34:48 -0300

    javascript com configs da lib typed

---
+++ public/js/pre-checkin/typed.js
@@ -0,0 +1,11 @@
+var typed = new Typed('#typed', {
+  strings: [
+    'Olá, boas-vindas ao nosso pré check-in!',
+    'Para começar, preencha os campos abaixo com seus dados pessoais.',
+  ],
+  stringsElement: null,
+  typeSpeed: 30,
+  backSpeed: 30,
+  loop: true,
+  loopCount: Infinity,
+});
