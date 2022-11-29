f8c8a91c9e5ff9cbf1d9b479b12e3e9edf523b04
gabrieldasilvadev <gabriel.dsilva@sptech.school>
2022-11-29 02:58:14 -0300

    Implemented, hospital name and formate date

--- src/models/preCheckinModel.js
+++ src/models/preCheckinModel.js
@@ -1,16 +1,17 @@
 var database = require('../database/config');
 
+
 function relizarPreCheckin(
   nome,
   email,
-  rg,
   cpf,
   dataCheckin,
   idade,
   optionTipoConsulta,
-  fkHospital
+  fkHospital,
+  optionNomeHospital
 ) {
-  var instrucao = `INSERT INTO dbo.pre_check_in (nome, cpf, rg, idade, email, data_cadastro, fkhospital, [motivo]) VALUES ('${nome}', '${cpf}', '${rg}', '${idade}', '${email}', '${dataCheckin}', ${fkHospital}, '${optionTipoConsulta}');`;
+  var instrucao = `INSERT INTO dbo.pre_check_in (nome, cpf, idade, email, data_cadastro, fkhospital, [motivo], nome_hospital) VALUES ('${nome}', '${cpf}', '${idade}', '${email}', '${dataCheckin}', ${fkHospital}, '${optionTipoConsulta}', '${optionNomeHospital}');`;
 
   return database.executar(instrucao);
 }
